7a98803315fff6aae1780ffc59515f32b3e20300
Grant Bakker <grant@bakker.pw>
2014-02-26 11:05:00 -0800

    Added info to home and minecraft pages

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,46 @@
+var async = require('async');
+var request = require('request');
+var moment = require('moment');
+
+var serverDomain = 'server.bakker.pw:9980';
+
 module.exports = function(req, res){
-  res.render('home', { title: 'Home' });
+  async.parallel({
+    disk: function(diskCallback){
+      request('http://' + serverDomain + '/status/disk', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var diskUsage = JSON.parse(body);
+          var symbol = 'GB';
+          var size = 1000 * 1000;
+          diskUsage.used /= size;
+          diskUsage.available /= size;
+          var percent = Math.round((diskUsage.used / (diskUsage.used + diskUsage.available)) * 100);
+          var text = percent + '% ( ' + Math.round(diskUsage.used) + symbol + ' / ' + Math.round(diskUsage.used + diskUsage.available) + symbol + ' )';
+          diskCallback(null, text);
+        }else{
+          diskCallback(null, 'unavailable!');
+        }
+      });
+    },
+    online: function(onlineCallback){
+      request('http://' + serverDomain + '/', function (error, response, body) {
+        onlineCallback(null, !error && response.statusCode == 200);
+      });
+    },
+    updated: function(updatedCallback){
+      request('http://' + serverDomain + '/status/date', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var text = moment(body).fromNow();
+          updatedCallback(null, text);
+        }else{
+          updatedCallback(null, 'unavailable!');
+        }
+      });
+    }
+  },function(error, statusResults){
+    res.render('home',{
+      title: 'Home',
+      status: statusResults
+    });
+  });
 };
--- routes/minecraft.js
+++ routes/minecraft.js
@@ -1,3 +1,67 @@
-module.exports = function(req, res){
-  res.render('minecraft', { title: 'Minecraft', link: 'map.bakker.pw' });
+var async = require('async');
+var request = require('request');
+var moment = require('moment');
+
+var serverDomain = 'server.bakker.pw:9980';
+
+var getPlayerList = function(body, callback){
+  try{
+    var lines = body.split('\n');
+    while(lines[lines.length - 1] === ''){
+      lines.pop();
+    }
+    var countText = lines.shift();
+    var regex = /^There are (\d+)\/(\d+) players online:$/
+    var result = countText.match(regex);
+    var playerCount = parseInt(result[1], 10);
+    var players = [];
+    for(var i = 0; i < playerCount; i++){
+      players.push({
+        username : lines[i]
+      });
+    }
+    callback(null, players);
+  }catch(error){
+    callback(null, []);
+  }
 };
+
+module.exports = function(req, res){
+  async.parallel({
+    survival: function(survivalCallback){
+      request('http://' + serverDomain + '/status/minecraft-survival', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            getPlayerList(body, survivalCallback);
+        }else{
+          survivalCallback(null, []);
+        }
+      });
+    },
+    creative: function(creativeCallback){
+      request('http://' + serverDomain + '/status/minecraft-creative', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          getPlayerList(body, creativeCallback);
+        }else{
+          creativeCallback(null, []);
+        }
+      });
+    },
+    updated: function(updatedCallback){
+      request('http://' + serverDomain + '/status/minecraft-date', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var text = moment(body).fromNow();
+          updatedCallback(null, text);
+        }else{
+          updatedCallback(null, 'unavailable!');
+        }
+      });
+    }
+  },function(error, statusResults){
+    res.render('minecraft', {
+      title: 'Minecraft',
+      link: 'map.bakker.pw',
+      status: statusResults
+    });
+  });
+};
+
