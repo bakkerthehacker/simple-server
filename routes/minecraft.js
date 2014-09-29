4a6461da1d031eb8fe2184fe0ae3e5f437735360
Grant Bakker <grant@bakker.pw>
2014-09-28 17:10:07 -0700

    Added minecraft query stats

--- routes/minecraft.js
+++ routes/minecraft.js
@@ -1,37 +1,53 @@
 var async = require('async');
 var request = require('request');
 var moment = require('moment');
+var Query = require('mcquery');
 
 var serverDomain = 'server.bakker.pw:9980';
 
+function queryServerStats(port, callback) {
+	
+	var query = new Query('localhost', port);
+
+	query.connect(function(connectError){
+		if(connectError){
+			//return callback(connectError)
+			return callback(null, {'online':false});
+		}
+		query.full_stat(function(statError, stat){
+		//query.basic_stat(function(statError, stat){
+			if(statError){
+				//return callback(statError)
+				return callback(null, {'online':false});
+			}
+			//console.log(stat)
+			query.close();
+			stat.online = true;
+			setImmediate(function(){
+				callback(null, stat);
+			});
+		});
+	});
+}
+
 module.exports = function(req, res){
 	async.parallel({
-//		survivalPlayers: function(parallelCallback){
-//			child_process.spawn('sudo', ['-u minecraft', 'msm survival connected']).stdout.on('data', function(data){
-//				var playerString = data.toString();
-//				parallelCallback(null, diskString);
-//			});
-//		}
-		survivalPlayers: function(parallelCallback){
-			setImmediate(function(){
-				parallelCallback(null, []);
-			});
+		survivalStats: function(parallelCallback){
+			queryServerStats(25561, parallelCallback);
 		},
-		creativePlayers: function(parallelCallback){
-			setImmediate(function(){
-				parallelCallback(null, []);
-			});
+		creativeStats: function(parallelCallback){
+			queryServerStats(25560, parallelCallback);
 		},
 	}, function(parallelError, results){
 		res.render('minecraft', {
 			title: 'Minecraft',
 			link: 'map.bakker.pw',
-			status: {
+			servers: {
 				survival: {
 					name: 'Survival',
 					link: 'survival.bakker.pw',
 					rules: ['Survive'],
-					players: results.survivalPlayers
+					stats: results.survivalStats
 				},
 				creative: {
 					name: 'Creative',
@@ -41,7 +57,7 @@ module.exports = function(req, res){
 						'Do not break things that are not yours',
 						'Do not use the plugins unless you know what you are doing',
 					],
-					players: results.creativePlayers
+					stats: results.creativeStats
 				}
 			}
 		});
